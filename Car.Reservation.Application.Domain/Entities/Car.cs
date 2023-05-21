7e025a9286547265df1cc967301479f12be1c728
shamimkasim <shamim2002@gmail.com>
2023-05-21 10:10:18 -0300

    Car model

--- Car.Reservation.Application.Domain/Entities/Car.cs
+++ Car.Reservation.Application.Domain/Entities/Car.cs
@@ -7,14 +7,7 @@ using System.Threading.Tasks;
 namespace CarReservation.Domain.Entities
 {
     public class Car
-    {
-        //public string Id { get; set; }
-        //public string Make { get; set; }
-        //public string Model { get; set; }
-        //public string Name { get; set; }
-        //public bool Reserved { get; set; }
-        //public int? ReservationId { get; set; }
-        //public Reservation Reservation { get; set; }
+    {       
         public string Id { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
